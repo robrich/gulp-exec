cc4338841c698709fbdf65bc8c8880e592deba8e
Rob Richardson <robrich@robrich.org>
2018-07-04 11:31:28 -0700

    fix #45: doesn't properly prepend path with .bin folder

--- prependPath.js
+++ prependPath.js
@@ -10,7 +10,7 @@ function prependPath(env) {
 	// FRAGILE: ASSUME: this is a top-level module, hasn't been de-duped into a sub-directory
 	// TODO: if this becomes a problem, resolve node_modules/.bin using npm-which or npm-path or similar or invite users to use npx <command>
 	var oldPath = env.PATH;
-	var newPath = path.resolve(path.join(__dirname, '..', '..', '.bin'));
+	var newPath = path.resolve(path.join(__dirname, '..', '.bin'));
 	if (oldPath.indexOf(newPath) === -1) {
 		newPath += path.delimiter;
 		newPath += oldPath;
